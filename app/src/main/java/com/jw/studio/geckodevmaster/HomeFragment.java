cd5fae92d03ff696c3ed3fbb5ae0e66fb100d7cc
kit rio <mario64aq@gmail.com>
2020-01-04 19:44:13 +0900

    Refactor fragment : HomeFragment.java

--- app/src/main/java/com/jw/studio/geckodevmaster/HomeFragment.java
+++ app/src/main/java/com/jw/studio/geckodevmaster/HomeFragment.java
@@ -14,32 +14,38 @@ import androidx.annotation.Nullable;
 import androidx.core.content.res.ResourcesCompat;
 
 import java.util.Locale;
-import java.util.Objects;
 
 public class HomeFragment extends Fragment {
 
     private GeckoViewActivity activity;
+    private FragmentManager fragmentManager;
     @Override
     public void onAttach(Context context){
         super.onAttach(context);
-        activity = (GeckoViewActivity)getActivity();
+        if( context instanceof GeckoViewActivity){
+            activity = (GeckoViewActivity)context;
+        }
     }
-    @Nullable
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        fragmentManager = activity.getFragmentManager();
+    }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.home_fragment , container, false);
         Button googleBtn = rootView.findViewById(R.id.button_google);
         Button youtubeBtn = rootView.findViewById(R.id.button_youtube);
         Button localBtn = rootView.findViewById(R.id.button_localurl);
-
-        FragmentManager fragmentManager = activity.getFragmentManager();
-
+        Drawable topDrawable;
         googleBtn.setText("Google");
         googleBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                activity.mTabSessionManager.getCurrentSession().loadUri("https://google.com");
-                fragmentManager.beginTransaction().remove(HomeFragment.this).commitAllowingStateLoss();
+                activity.mTabSessionManager.getCurrentSession().loadUri("https://www.google.com");
+                fragmentManager.beginTransaction().hide(HomeFragment.this).commitAllowingStateLoss();
             }
         });
 
@@ -48,24 +54,24 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 activity.mTabSessionManager.getCurrentSession().loadUri("https://m.youtube.com");
-                fragmentManager.beginTransaction().remove(HomeFragment.this).commitAllowingStateLoss();
+                fragmentManager.beginTransaction().hide(HomeFragment.this).commitAllowingStateLoss();
             }
         });
 
-        Drawable topdrawable;
+
         if (Locale.getDefault().getLanguage().equals("ko")) {
-            topdrawable = ResourcesCompat.getDrawable(getResources(), R.drawable.fav_naver, null);
+            topDrawable = ResourcesCompat.getDrawable(getResources(), R.drawable.fav_naver, null);
         }else{
-            topdrawable = ResourcesCompat.getDrawable(getResources(),R.drawable.fav_duckduckgo,null);
+            topDrawable = ResourcesCompat.getDrawable(getResources(),R.drawable.fav_duckduckgo,null);
         }
-        Objects.requireNonNull(topdrawable).setBounds(0,-5,0,0);
-        localBtn.setCompoundDrawablesWithIntrinsicBounds(null,topdrawable,null,null);
+        topDrawable.setBounds(0,-5,0,0);
+        localBtn.setCompoundDrawablesWithIntrinsicBounds(null,topDrawable,null,null);
         localBtn.setText(getString(R.string.urlname));
         localBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 activity.mTabSessionManager.getCurrentSession().loadUri(getString(R.string.initurl));
-                fragmentManager.beginTransaction().remove(HomeFragment.this).commitAllowingStateLoss();
+                fragmentManager.beginTransaction().hide(HomeFragment.this).commitAllowingStateLoss();
             }
         });
         return rootView;
