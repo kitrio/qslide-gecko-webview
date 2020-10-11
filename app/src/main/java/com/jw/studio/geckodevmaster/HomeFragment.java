daab52031b20afc8a813484e4e410c907250871d
kit rio <mario64aq@gmail.com>
2020-10-11 22:16:53 +0900

    Apply Databinding in homefragment

--- app/src/main/java/com/jw/studio/geckodevmaster/HomeFragment.java
+++ app/src/main/java/com/jw/studio/geckodevmaster/HomeFragment.java
@@ -10,16 +10,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.jw.studio.geckodevmaster.databinding.HomeFragmentBinding;
+
 import java.util.Locale;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.res.ResourcesCompat;
+import androidx.databinding.DataBindingUtil;
 
 public class HomeFragment extends Fragment {
 
     private GeckoViewActivity activity;
     private FragmentManager fragmentManager;
+    private HomeFragmentBinding homeFragmentBinding;
 
     @Override
     public void onAttach(Context context) {
@@ -37,22 +41,21 @@ public class HomeFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.home_fragment, container, false);
-        Button googleBtn = rootView.findViewById(R.id.button_google);
-        Button youtubeBtn = rootView.findViewById(R.id.button_youtube);
-        Button localBtn = rootView.findViewById(R.id.button_localurl);
-        Button facebookBtn = rootView.findViewById(R.id.button_facebook);
-        Button instagramBtn =rootView.findViewById(R.id.button_instagram);
-        Button twitterBtn =rootView.findViewById(R.id.button_twitter);
+        homeFragmentBinding = DataBindingUtil.inflate(inflater,R.layout.home_fragment, container, false);
+        View rootView = homeFragmentBinding.getRoot();
+        Button googleBtn = homeFragmentBinding.buttonGoogle;
+        Button youtubeBtn = homeFragmentBinding.buttonYoutube;
+        Button localBtn = homeFragmentBinding.buttonLocalurl;
+        Button facebookBtn = homeFragmentBinding.buttonFacebook;
+        Button instagramBtn = homeFragmentBinding.buttonInstagram;
+        Button twitterBtn = homeFragmentBinding.buttonTwitter;
         Drawable topDrawable;
 
-        googleBtn.setText("Google");
         googleBtn.setOnClickListener(v -> {
             activity.tabSessionManager.getCurrentSession().loadUri("https://www.google.com");
             fragmentManager.beginTransaction().hide(HomeFragment.this).commitAllowingStateLoss();
         });
 
-        youtubeBtn.setText("Youtube");
         youtubeBtn.setOnClickListener(v -> {
             activity.tabSessionManager.getCurrentSession().loadUri("https://m.youtube.com");
             fragmentManager.beginTransaction().hide(HomeFragment.this).commitAllowingStateLoss();
@@ -67,30 +70,25 @@ public class HomeFragment extends Fragment {
         }
         topDrawable.setBounds(0, -5, 0, 0);
         localBtn.setCompoundDrawablesWithIntrinsicBounds(null, topDrawable, null, null);
-        localBtn.setText(getString(R.string.urlname));
         localBtn.setOnClickListener(v -> {
             activity.tabSessionManager.getCurrentSession().loadUri(getString(R.string.initurl));
             fragmentManager.beginTransaction().hide(HomeFragment.this).commitAllowingStateLoss();
         });
 
-        facebookBtn.setText(getString(R.string.facebook));
         facebookBtn.setOnClickListener(v -> {
             activity.tabSessionManager.getCurrentSession().loadUri("https://m.facebook.com");
             fragmentManager.beginTransaction().hide(HomeFragment.this).commitAllowingStateLoss();
         });
 
-        instagramBtn.setText(getString(R.string.instagram));
         instagramBtn.setOnClickListener(v -> {
             activity.tabSessionManager.getCurrentSession().loadUri("https://www.instagram.com");
             fragmentManager.beginTransaction().hide(HomeFragment.this).commitAllowingStateLoss();
         });
 
-        twitterBtn.setText(getString(R.string.twitter));
         twitterBtn.setOnClickListener(v -> {
             activity.tabSessionManager.getCurrentSession().loadUri("https://mobile.twitter.com");
             fragmentManager.beginTransaction().hide(HomeFragment.this).commitAllowingStateLoss();
         });
-
 
         return rootView;
     }
